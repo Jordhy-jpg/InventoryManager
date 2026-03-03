f3581d5ed60c6bc9364ff067af4adbdb0563a948
Jordhy <alexanderwibisonoj@gmail.com>
2026-03-03 13:52:02 +0700

    finishing touches, again

--- InventoryManager/Main/settings.py
+++ InventoryManager/Main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRETKEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'inventory-manager-delta-gilt.vercel.app']
 
 
 # Application definition
